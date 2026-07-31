6bb01f4ebc08bea4ae337e1ae1e59bf1f27fb241
Manevres <manevres@gmail.com>
2026-07-31 12:42:02 +0300

    Настройка секретов

--- src/test/java/auto/tests/testdata/TestData.java
+++ src/test/java/auto/tests/testdata/TestData.java
@@ -3,10 +3,10 @@ package auto.tests.testdata;
 import java.util.UUID;
 
 public class TestData {
-    public static final String PASSWORD = "buben12345";
-    public static final String SHORT_PASSWORD = "buben1";
-    public static final String INVALIDATE_PASSWORD = "buben";
-    public static final String WRONG_PASSWORD = "buben12346";
+    public static final String PASSWORD = getEnvOrDefault("PASSWORD", "buben12345");
+    public static final String SHORT_PASSWORD = getEnvOrDefault("PASSWORD", "buben1");
+    public static final String INVALIDATE_PASSWORD = getEnvOrDefault("INVALIDATE_PASSWORD", "buben");
+    public static final String WRONG_PASSWORD = getEnvOrDefault("WRONG_PASSWORD", "buben12346");
     public static final String EMPTY_PASSWORD = "";
     public static final String NAME = "name";
     public static final String EMPTY_NAME = "";
@@ -31,5 +31,9 @@ public class TestData {
 
     public static String generateEmailWithCaps() {
         return "BUBEN" + UUID.randomUUID() + "@MAIL.RU";
+    }
+
+    private static String getEnvOrDefault(String envName, String defaultValue) {
+        return System.getenv().getOrDefault(envName, defaultValue);
     }
 }
