9a5c6b58b98788cdf91b33edf76d47763286e1a6
Manevres <manevres@gmail.com>
2026-09-07 16:37:10 +0300

    убрал из gitignore BaseTest

---
+++ src/test/java/auto/tests/base/BaseTest.java
@@ -0,0 +1,93 @@
+package auto.tests.base;
+
+import auto.tests.config.Config;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    void openPage() throws Exception {
+
+        String browser = System.getenv().getOrDefault("BROWSER", "chrome").toLowerCase();
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        boolean ci = System.getenv("CI") != null;
+        if (remoteUrl != null && !remoteUrl.isBlank()) {
+            driver = createRemoteDriver(browser, remoteUrl);
+        } else {
+            driver = createLocalDriver(browser, ci);
+        }
+
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.get(Config.URL);
+    }
+
+    private WebDriver createLocalDriver(String browser, boolean ci) {
+
+        switch (browser) {
+
+            case "chrome":
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                if (ci) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                }
+
+                return new ChromeDriver(chromeOptions);
+
+            default:
+
+                throw new IllegalArgumentException(
+                        "Unsupported browser: " + browser
+                );
+        }
+    }
+
+    private WebDriver createRemoteDriver(
+            String browser,
+            String remoteUrl
+    ) throws Exception {
+
+        switch (browser) {
+
+            case "firefox":
+
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                return new RemoteWebDriver(new URL(remoteUrl), firefoxOptions);
+
+            case "edge":
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+                return new RemoteWebDriver(new URL(remoteUrl), edgeOptions);
+
+            case "chrome":
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+                return new RemoteWebDriver(new URL(remoteUrl), chromeOptions);
+
+            default:
+                throw new IllegalArgumentException("Unsupported remote browser: " + browser);
+        }
+    }
+
+    @AfterEach
+    void endTest() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
